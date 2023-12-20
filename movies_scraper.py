3dd772d70e97b6b5219858c333b26b11ab093ea7
Islam Bn <99047255+is-bn@users.noreply.github.com>
2023-12-20 10:08:45 +0100

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -1,11 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-url_list = {}
-api_key = "ENTER YOUR API KEY HERE"
-
-
 def search_movies(query):
     movies_list = []
     movies_details = {}
@@ -15,15 +10,14 @@ def search_movies(query):
         if movie:
             movies_details["id"] = f"link{movies.index(movie)}"
             movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
-            url_list[movies_details["id"]] = movie['href']
-        movies_list.append(movies_details)
-        movies_details = {}
+            movies_details["link"] = movie['href'] # Store the direct link to the movie
+            movies_list.append(movies_details)
+            movies_details = {}
     return movies_list
-
 
 def get_movie(query):
     movie_details = {}
-    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
+    movie_page_link = BeautifulSoup(requests.get(f"{query}").text, "html.parser") # Use the direct link
     if movie_page_link:
         title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
         movie_details["title"] = title
@@ -32,10 +26,6 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-            response = requests.get(url)
-            link = response.json()
-            final_links[f"{i.text}"] = link['shortenedUrl']
+            final_links[f"{i.text}"] = i['href'] # Store the direct links without shortening
         movie_details["links"] = final_links
     return movie_details
-
